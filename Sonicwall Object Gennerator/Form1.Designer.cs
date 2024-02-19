fbfe9cde0f450f51005207e49db7714eed123baf
Geir Bakke <geir@nettdrift.no>
2024-02-19 13:42:09 +0100

    Add project files.

---
+++ Sonicwall Object Gennerator/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Sonicwall_Object_Gennerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Download = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Download
+            // 
+            this.btn_Download.Location = new System.Drawing.Point(12, 12);
+            this.btn_Download.Name = "btn_Download";
+            this.btn_Download.Size = new System.Drawing.Size(90, 27);
+            this.btn_Download.TabIndex = 0;
+            this.btn_Download.Text = "Download";
+            this.btn_Download.UseVisualStyleBackColor = true;
+            this.btn_Download.Click += new System.EventHandler(this.btn_Download_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(309, 800);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(327, 45);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(335, 800);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarning.Location = new System.Drawing.Point(119, 15);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(0, 24);
+            this.lblWarning.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 857);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_Download);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Download;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label lblWarning;
+    }
+}
+
---
+++ Sonicwall Object Gennerator/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+using System.Security.Policy;
+using System.Xml.Linq;
+
+
+
+
+
+namespace Sonicwall_Object_Gennerator
+{
+  
+
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            string result = Path.GetTempPath();
+            Console.WriteLine(result);
+
+        }
+     
+        private async void btn_Download_Click(object sender, EventArgs e)
+        {
+            btn_Download.Enabled = false;
+            lblWarning.Text = "Jobber!";
+           
+            using (WebClient wc = new WebClient())
+    {
+                
+                await wc.DownloadFileTaskAsync(
+                    // Param1 = Link of file
+                     new System.Uri("https://download.microsoft.com/download/B/2/A/B2AB28E1-DAE1-44E8-A867-4987FE089EBE/msft-public-ips.csv"),
+                    // Param2 = Path to save
+                    Path.GetTempPath()+"msft-public-ips.csv"
+                );
+            }
+            richTextBox2.AppendText("address-group Microsoft_WAN");
+            String line;
+            try
+            {
+                
+
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(Path.GetTempPath() + "msft-public-ips.csv");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+
+                    string[] TMP = line.Split(',').Select(str => str.Trim()).ToArray();
+                    string[] IP_Subnett = TMP.First().Split('/');
+
+                    IPAddress address;
+                    if (IPAddress.TryParse(IP_Subnett.First(), out address))
+                    {
+                        switch (address.AddressFamily)
+                        {
+                            case System.Net.Sockets.AddressFamily.InterNetwork:
+                                // This is IPv4 address
+                                richTextBox1.AppendText(Environment.NewLine + "address - object " + "\"MS " + TMP.First() + "\"");
+                                richTextBox2.AppendText(Environment.NewLine + "address - object " + "\"MS " + TMP.First() + "\"");
+                                
+                                richTextBox1.AppendText(Environment.NewLine + "network " + IP_Subnett.First() + " " + getSubnetAddressFromIPNetMask(IP_Subnett.Last()));
+                                richTextBox1.AppendText(Environment.NewLine + "zone WAN");
+                                richTextBox1.AppendText(Environment.NewLine + "exit" + Environment.NewLine); 
+
+                                //richTextBox1.AppendText(Environment.NewLine + "This is IPv4 address");
+                                //richTextBox1.AppendText(Environment.NewLine + IP_Subnett.First());
+                                //richTextBox1.AppendText(Environment.NewLine + getSubnetAddressFromIPNetMask(IP_Subnett.Last()));
+                                break;
+                            case System.Net.Sockets.AddressFamily.InterNetworkV6:
+                                //richTextBox1.AppendText(Environment.NewLine + "This is IPv6 address");
+                                //richTextBox1.AppendText(Environment.NewLine + IP_Subnett.First());
+                                //richTextBox1.AppendText(Environment.NewLine + getSubnetAddressFromIPNetMask(IP_Subnett.Last()));
+
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    
+                    //Read the next line
+                    line = sr.ReadLine();
+
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine("Exception: " + f.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+            richTextBox2.AppendText(Environment.NewLine + "exit" );
+            btn_Download.Enabled = true;
+            lblWarning.Text = "Ferdig!";
+
+        }
+    
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            // Display a MsgBox asking the user to save changes or abort.
+            if (MessageBox.Show("Do you want to delete downloaded file?", "Cleanup",
+               MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // Call method to delete file...
+                File.Delete(Path.GetTempPath() + "msft-public-ips.csv");
+
+            }
+            
+        }
+
+        private static bool IsIPAddress(string ipAddress)
+        {
+            bool retVal = false;
+
+            try
+            {
+                IPAddress address;
+                retVal = IPAddress.TryParse(ipAddress, out address);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return retVal;
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+            IPAddress address;
+            if (IPAddress.TryParse("192.168.168.0", out address))
+            {
+                switch (address.AddressFamily)
+                {
+                    case System.Net.Sockets.AddressFamily.InterNetwork:
+                        // This is IPv4 address
+                        richTextBox1.AppendText(Environment.NewLine + "This is IPv4 address");
+                        break;
+                    case System.Net.Sockets.AddressFamily.InterNetworkV6:
+                        // This is IPv6 address
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+
+
+        //This Function is Used to get Subnet basede on NetMask(i.e 0-32)
+        public string getSubnetAddressFromIPNetMask(string netMask)
+        {
+            string subNetMask = string.Empty;
+            int calSubNet = 0;
+            double result = 0;
+            if (!string.IsNullOrEmpty(netMask))
+            {
+                calSubNet = 32 - Convert.ToInt32(netMask);
+                if (calSubNet >= 0 && calSubNet <= 8)
+                {
+                    for (int ipower = 0; ipower < calSubNet; ipower++)
+                    {
+                        result += Math.Pow(2, ipower);
+                    }
+                    double finalSubnet = 255 - result;
+                    subNetMask = "255.255.255." + Convert.ToString(finalSubnet);
+                }
+                else if (calSubNet > 8 && calSubNet <= 16)
+                {
+                    int secOctet = 16 - calSubNet;
+
+                    secOctet = 8 - secOctet;
+
+                    for (int ipower = 0; ipower < secOctet; ipower++)
+                    {
+                        result += Math.Pow(2, ipower);
+                    }
+                    double finalSubnet = 255 - result;
+                    subNetMask = "255.255." + Convert.ToString(finalSubnet) + ".0";
+                }
+                else if (calSubNet > 16 && calSubNet <= 24)
+                {
+                    int thirdOctet = 24 - calSubNet;
+
+                    thirdOctet = 8 - thirdOctet;
+
+                    for (int ipower = 0; ipower < thirdOctet; ipower++)
+                    {
+                        result += Math.Pow(2, ipower);
+                    }
+                    double finalSubnet = 255 - result;
+                    subNetMask = "255." + Convert.ToString(finalSubnet) + ".0.0";
+                }
+                else if (calSubNet > 24 && calSubNet <= 32)
+                {
+                    int fourthOctet = 32 - calSubNet;
+
+                    fourthOctet = 8 - fourthOctet;
+
+                    for (int ipower = 0; ipower < fourthOctet; ipower++)
+                    {
+                        result += Math.Pow(2, ipower);
+                    }
+                    double finalSubnet = 255 - result;
+                    subNetMask = Convert.ToString(finalSubnet) + ".0.0.0";
+                }
+            }
+
+            return subNetMask;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
